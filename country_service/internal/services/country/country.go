bcbea250c485a7fdc7c2367a2a4b5ada85c7b99d
LiveisFpv <nasonovnicolai@gmail.com>
2025-02-25 19:15:56 +0300

    add: funcs country service with logging

--- country_service/internal/services/country/country.go
+++ country_service/internal/services/country/country.go
@@ -39,26 +39,106 @@ func New(
 }
 
 // Add_Country implements countrygrpc.Country.
-func (c *CountryService) Add_Country(ctx context.Context, country_title, country_capital, country_area string) (country *models.Country, err error) {
-	panic("unimplemented")
+func (c *CountryService) Add_Country(
+	ctx context.Context,
+	country_title,
+	country_capital,
+	country_area string) (
+	country *models.Country,
+	err error) {
+	const op = "Country.Create"
+	log := c.log.WithFields(
+		logrus.Fields{
+			"op":      op,
+			"title":   country_title,
+			"capital": country_capital,
+			"area":    country_area,
+		},
+	)
+	log.Info("Start Create Country")
+
+	country, err = c.countryStorage.CreateCountry(ctx, country_title, country_capital, country_area)
+	if err != nil {
+		c.log.Error("failed to create country", err)
+		return nil, err
+	}
+
+	return country, nil
 }
 
 // Delete_CountrybyID implements countrygrpc.Country.
 func (c *CountryService) Delete_CountrybyID(ctx context.Context, country_id int) (err error) {
-	panic("unimplemented")
+	const op = "Country.Delete"
+	log := c.log.WithFields(
+		logrus.Fields{
+			"op": op,
+			"id": country_id,
+		},
+	)
+	log.Info("Start Delete Country")
+
+	err = c.countryStorage.DeleteCountrybyID(ctx, country_id)
+	if err != nil {
+		c.log.Error("failed to delete country", err)
+		return err
+	}
+	return nil
 }
 
 // Get_All_Country implements countrygrpc.Country.
 func (c *CountryService) Get_All_Country(ctx context.Context) (countries []*models.Country, err error) {
-	panic("unimplemented")
+	const op = "Country.GetAll"
+	log := c.log.WithFields(
+		logrus.Fields{
+			"op": op,
+		},
+	)
+	log.Info("Start Get ALL Country")
+
+	countries, err = c.countryStorage.GetAllCountry(ctx)
+	if err != nil {
+		c.log.Error("failed to get all countries", err)
+		return nil, err
+	}
+
+	return countries, nil
 }
 
 // Get_CountrybyID implements countrygrpc.Country.
 func (c *CountryService) Get_CountrybyID(ctx context.Context, country_id int) (country *models.Country, err error) {
-	panic("unimplemented")
+	const op = "Country.GetbyID"
+	log := c.log.WithFields(
+		logrus.Fields{
+			"op": op,
+			"id": country_id,
+		},
+	)
+	log.Info("Start Get by ID Country")
+	country, err = c.countryStorage.GetCountrybyID(ctx, country_id)
+	if err != nil {
+		c.log.Error("failed to get country by id", err)
+		return nil, err
+	}
+	return country, nil
 }
 
 // Update_CountrybyID implements countrygrpc.Country.
 func (c *CountryService) Update_CountrybyID(ctx context.Context, country *models.Country) (err error) {
-	panic("unimplemented")
+	const op = "Country.Update"
+	log := c.log.WithFields(
+		logrus.Fields{
+			"op":      op,
+			"id":      country.Country_id,
+			"title":   country.Country_title,
+			"capital": country.Country_capital,
+			"area":    country.Country_area,
+		},
+	)
+	log.Info("Start Update country")
+	err = c.countryStorage.UpdateCountrybyID(ctx, country)
+	if err != nil {
+		c.log.Error("failed to update country", err)
+		return err
+	}
+	return nil
 }
