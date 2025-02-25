ea7dc315bfbae7259e7d3e1fb65d1c58901d76ef
LiveisFpv <nasonovnicolai@gmail.com>
2025-02-25 15:38:57 +0300

    Add: connect to pgSQL, close conn, storage, interface for work with pgSQL

--- country_service/internal/storage/storage.go
+++ country_service/internal/storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"country_service/internal/domain/models"
+	postgresql "country_service/internal/storage/postgreSQL"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/sirupsen/logrus"
+)
+
+type repo struct {
+	*postgresql.Queries
+	log *logrus.Logger
+}
+
+// Repository constructor
+func NewRepository(
+	pgxpool *pgxpool.Pool,
+	log *logrus.Logger,
+) Repository {
+	return &repo{
+		Queries: New(pgxpool),
+		log:     log,
+	}
+}
+
+// TODO sql statement and func
+// Func for work with DB
+type Repository interface {
+	GetCountrybyID(country_id int) (country *models.Country, err error)
+	GetAllCountry() (countries *[]models.Country, err error)
+	CreateCountry(country *models.Country) (country_id int, err error)
+	UpdateCountrybyID(country *models.Country) (err error)
+	DeleteCountrybyID(country_id int) (err error)
+}
